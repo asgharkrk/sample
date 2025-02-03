6c87f4c5a3f9aa01218a46496fc6f76f7ab52084
Asghar Nazir <asghar@syntaxtechs.com>
2025-02-03 12:29:22 -0500

    init

--- src/main/java/org/example/QTestApiClientCopy.java
+++ src/main/java/org/example/QTestApiClientCopy.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import javax.net.ssl.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.security.cert.X509Certificate;
 import java.util.List;
 
 
@@ -19,7 +21,51 @@ public class QTestApiClientCopy {
 
     private static final String AUTH_HEADER_VALUE = "Bearer <YOUR_BEARER_TOKEN>";
 
+
+    public static void setDisableSSLVerification(boolean disable) {
+        if (disable) {
+            try {
+                // Create a trust manager that does not validate certificate chains
+                TrustManager[] trustAllCerts = new TrustManager[]{
+                        new X509TrustManager() {
+                            @Override
+                            public X509Certificate[] getAcceptedIssuers() {
+                                return new X509Certificate[0];
+                            }
+                            @Override
+                            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                                // No need to implement.
+                            }
+                            @Override
+                            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                                // No need to implement.
+                            }
+                        }
+                };
+
+                // Install the all-trusting trust manager
+                SSLContext sc = SSLContext.getInstance("SSL");
+                sc.init(null, trustAllCerts, new java.security.SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+                // Create an all-trusting host name verifier
+                HostnameVerifier allHostsValid = new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                };
+                HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
     public static void linkTestCasesToRequirement(List<String> testCaseIds) throws IOException {
+        setDisableSSLVerification(true);
+
         if (testCaseIds == null || testCaseIds.isEmpty()) {
             System.err.println("No testCaseIds provided to link.");
             return;

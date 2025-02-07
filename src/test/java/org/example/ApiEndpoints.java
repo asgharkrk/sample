42da30d6b47773ceb5942a3089a9674506c6a21c
Asghar Nazir <asghar@syntaxtechs.com>
2025-02-07 15:25:32 -0500

    Payload

--- src/test/java/org/example/ApiEndpoints.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-// ApiEndpoints.java
-public class ApiEndpoints {
-    public static final String BASE_URL = "";
-    public static final String PROJECT_ID = "";
-    public static final String AUTO_TEST_LOGS = BASE_URL + "/projects/" + PROJECT_ID + "/auto-test-logs?type=automation";
-}
